81b55982c40b7c431263d3700c17f8a0e5ed4780
R0manYe <roman_y77@mail.ru>
2023-08-09 16:29:37 +0700

    IdentityFilialStation

---
+++ Vspt.BackEnd.Domain/Entity/FilialsSpr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vspt.Box.Data.Entities;
+
+namespace Vspt.BackEnd.Domain.Entity
+{
+    public class FilialsSpr 
+    {
+        public ushort Id { get; set; }
+        public string IdTxt { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
---
+++ Vspt.BackEnd.Domain/Entity/IdentityFilialStation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vspt.BackEnd.Domain.Entity
+{
+    internal class IdentityFilialStation
+    {
+        public uint FilialId { get; set; }
+        public string StationId { get; set; }
+    }
+}
